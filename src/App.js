1778f9457fb9ec3d3be95e423f612da9b824607f
Darren Wayn <darrenwayn98@gmail.com>
2022-06-22 20:51:57 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
+import { useState } from 'react';
 import FormInput from './components/FormInput';
-import { useState } from 'react'
 
 function App() {
-  const [username, setUsername] = useState('')
-  console.log(username)
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    birthday: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const inputs = [
+    {
+      id: 1,
+      name: 'birthday',
+      type: 'text',
+      placeholder: 'Birthday',
+      label: 'Birthday',
+    },
+    {
+      id: 2,
+      name: 'Username',
+      type: 'text',
+      placeholder: 'Username',
+      label: 'Username',
+    },
+    {
+      id: 2,
+      name: 'Email',
+      type: 'text',
+      placeholder: 'Email',
+      label: 'Email',
+    },
+    {
+      id: 3,
+      name: 'Fullname',
+      type: 'text',
+      placeholder: 'Full Name',
+      label: 'Fulllname',
+    },
+    {
+      id: 4,
+      name: 'password',
+      type: 'text',
+      placeholder: 'Password',
+      label: 'Password',
+    },
+    {
+      id: 5,
+      name: 'ConfirmPassword',
+      type: 'text',
+      placeholder: 'Confirm Password',
+      label: 'ConfirmPassword',
+    },
+  ]
+  const submithandler = (e) => {
+    e.prevent.default()
+  }
   return (
     <>
       <div className= 'container'>
         <div className="App">
-          <form> 
-            <FormInput placeholder="Username" setUsername={setUsername}/>
-            <FormInput placeholder="Email" />
-            <FormInput placeholder="Full Name" />
-            <FormInput placeholder="Something Else" />
+          <form onSubmit={submithandler}> 
+              {inputs.map((input => {
+                return <FormInput key={input.id} {...input} value={values[input.name]}/>
+              }))}
+            <button>Submit</button>
           </form>
         </div>
       </div>
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 
 const FormInput = (props) => {
-  const submithandler = (e) => {
-    e.prevent.default()
-  }
   return (
-    <div className='FormInput' onSubmit={submithandler}>
-        <label> </label>
-        <input 
+    <div className='FormInput'>
+        <input
             placeholder={props.placeholder} 
-            onChange={(e) => props.setUsername(e.target.value)} 
-         />
+            name={props.name}
+         /> 
     </div> 
   ) 
 }
