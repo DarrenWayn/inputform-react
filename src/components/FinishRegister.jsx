df3ef9bc4deaab53f22fd5ddb03dc15bd483ccd2
Darren Wayn <darrenwayn98@gmail.com>
2022-06-23 19:43:21 +0700

    finish register ui

---
+++ src/components/FinishRegister.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FinishRegister = () => {
+  return (
+    <div className='finishRegister'>
+        <h1>Thank you for reigstering</h1>
+    </div> 
+  )
+}
+
+export default FinishRegister
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FormInput = (props) => {
-  const {label, onChange, id, ...inputProps} = props
+  const [focused, setFocused] = useState(false)
+  const {label, onChange, errorMessage, id, ...inputProps} = props
+
+  const focusHandler = (e) => {
+    setFocused(true)
+  }
   return (
     <div className='FormInput'>
         <label>{label}</label>
         <input
-          {...inputProps} onChange={onChange}
+          {...inputProps} 
+          onChange={onChange}
+          onBlur={focusHandler}
+          onFocus={
+            () => inputProps.name==='confirmPassword' && setFocused(true)
+          }
+          focused={focused.toString()}
          /> 
+         <span>{errorMessage}</span>
     </div> 
   ) 
 }
