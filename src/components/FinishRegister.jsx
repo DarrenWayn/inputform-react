a8327bb70ef8e2def0951a0544ce70a6001819c0
Darren Wayn <darrenwayn98@gmail.com>
2022-06-23 20:45:56 +0700

    update test

--- src/components/FinishRegister.jsx
+++ src/components/FinishRegister.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FinishRegister = () => {
   return (
     <div className='finishRegister'>
-        <h1>Thank you for reigstering</h1>
+        <h1>Thank you for reigstering test</h1>
     </div> 
   )
 }
